ec28dcd170cff4c0de22b1e7773173105337998f
King-1025 <1543641386@qq.com>
2018-11-05 00:16:47 +0800

    修改config.sh

--- config.sh
+++ config.sh
@@ -1,25 +1,43 @@
 #!/usr/bin/env bash
 
-export PATH=$PATH:$HOME/self/bin
+export TERMUX_ROOT=/data/data/com.termux
+export TERMUX_PREFIX=$TERMUX_ROOT/files/usr
+export TERMUX_HOME=$TERMUX_ROOT/files/home
 
-#export OO=/sdcard/oo
-#export DL=/sdcard/Download
+export SELF=$TERMUX_HOME/self
+export PATH=$PATH:$SELF/bin:$SELF/sbin:$SELF/bin/busybox
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SELF/lib:$SELF/lib64
+
+export SCRIPT=$SELF/bin/script
+export KALI_HOME=$TERMUX_HOME/kali-arm64
+
 export OO=/storage/sdcard0/O_o
-export DL=/storage/sdcard0/Download
-export SC=$HOME/self/bin/script
 
 alias 91="91_spider"
 alias vse="if [ -f out.html ];then cp out.html $OO|android view -f $OO/out.html;else echo Sorry,not found out.html!;fi"
 alias v91="91 -r 0 3 -p 10 -ll i -ls middle -t html -o out.html;vse"
-alias oo="cd $OO"
-alias dl="cd $DL"
-alias sc="cd $SC"
+alias sc="cd $SCRIPT"
 alias gvs="git status"
 alias gaa="git add ."
 alias gm="git commit -m"
 alias uf="update_termux_font"
 alias fhp="find_httpd_pid"
 alias kh="kill_httpd"
+alias klh='cd $KALI_HOME/root'
+alias kali="exec_command_by_proot $KALI_HOME" 
+alias skl="kali bash"
+
+function exec_command_by_proot()
+{
+  if [ $# -gt 1 ]; then
+    local sysdir=$1
+    shift 1
+    unset LD_PRELOAD
+    proot --link2symlink -0 -r $sysdir -b /dev/ -b /sys/ -b /proc/ -b $TERMUX_HOME:/termux -w /root /usr/bin/env -i HOME=/root USER=root TERM="xterm-256color" LANG=en_US.UTF-8 PATH=/bin:/usr/bin:/sbin:/usr/sbin $@
+  else
+    echo "need at least 2 arguments!"
+  fi
+}
 
 function solve_vim_charset()
 {
@@ -32,8 +50,8 @@ function update_termux_font()
 { 
   if [ $# -eq 1 ]; then
      if [ -e $1 ];then
-        cp -fr "$1" "$HOME/.termux/font.ttf";
-	termux-reload-settings
+        cp -fr "$1" "$TERMUX_HOME/.termux/font.ttf";
+      	termux-reload-settings
      else
 	echo "not exist $1"
      fi
