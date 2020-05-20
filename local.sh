4cab1e83b731eea79c2349935940ca0634abbf44
King-1025 <1543641386@qq.com>
2020-05-20 14:11:17 +0800

    修改local安装流程

--- local.sh
+++ local.sh
@@ -1,82 +1,96 @@
 #!/usr/bin/env bash
 
-SELF=self
+SELF=$HOME/self
 BIN=$SELF/bin
-SCRIPT=$BIN/script
 LIB=$SELF/lib
+SCRIPT=$BIN/script
 
-LINK_RULE="local config kalinethunter log ohMyzsh"
+GIT_URL="https://github.com/King-1025/self-script.git"
 
-function add_config()
+declare -a MKDIR_LIST=($SELF $BIN $LIB)
+declare -a UN_LINK_LIST=(log ohMyzsh local config kalinethunter)
+
+function app()
 {
-  if [ $# -eq 2 ]; then
-     local c="$1/$SCRIPT/config.sh"
-     local f="$1/$2"
-     if [ -e "$f" ]; then
-	local r=$(sed -n "/O_o/p" "$f")
-	#echo $r
-	if [ "$r" != "" ]; then
-           return
-        fi
-     fi
-     echo "#[O_o]" >> "$f"
-     echo ". $c" >> "$f"
-     echo -e "\033[5;36mconfig -> $c $2\033[0m"
-  fi
+   echo "bootstrap... "
+   check_tool git
+   check_home
+   make_dirs
+   get_script
+   link_script
+   add_config ".bashrc" ".zshrc"
 }
 
-function link_script()
+function check_home()
 {
-   if [ $# -eq 1 ]; then
-    for s in $1/$SCRIPT/*.sh; do
-      local t=$(echo $(basename "$s")|awk -F "." '{print $1}')
-      eval "echo \"$LINK_RULE\"|grep -qvE \"$t\""
-      [ $? -eq 0 ] && echo -e "\033[5;32mlink -> $t\033[0m" && ln -sf "$s" "$1/$BIN/$t" && continue
-      echo -e "\033[5;33mignore -> $t\033[0m"
-      rm -rf "$1/$BIN/$t"
-    done
-   fi
-}
-
-function fix_script()
-{
-   if [ $# -eq 1 ]; then
-     local cur=$(pwd)
-     if [ "$cur" != "$1/$SCRIPT" ]; then
-        echo -e "\033[5;37mfix -> $1/$SCRIPT\033[0m"
-        mv "$cur" "$1/$SCRIPT"
-     fi
+   if [ -z $HOME ]; then
+      echo "\$HOME is not defined! please set it firstly"
+      exit 1
    fi
 }
 
 function make_dirs()
 {
-   if [ $# -eq 1 ]; then
-      for p in $1/{$SELF,$BIN,$LIB}; do
-          if [ -e $p ]; then
-             echo -e "\033[5;34mskip -> $p\033[0m"
+   for path in "${MKDIR_LIST[@]}"; do
+          if [ -e $path ]; then
+             echo -e "\033[5;34mskip -> $path\033[0m"
 	  else
-	     echo -e "\033[5;35mmkdir -> $p\033[0m"
-	     mkdir "$p" || exit 1
+	     echo -e "\033[5;35mmkdir -> $path\033[0m"
+	     mkdir "$path" || exit 1
 	  fi
-      done
-   fi
+   done
 }
 
-function bootstrap()
+function check_tool()
 {
-   local loc=$HOME
-   if [ -z $loc ]; then
-      echo "HOME is not defined! please set it firstly"
-      exit 0
-   fi
-   make_dirs "$loc"
-   fix_script "$loc"
-   link_script "$loc"
-   add_config "$loc" ".bashrc"
-   add_config "$loc" ".zshrc"
+    local list=""
+    for tool in "$@"; do
+       echo checking $tool...
+       which $tool 2>/dev/null
+       if [ $? -ne 0 ]; then
+	  echo "$tool not exist, add!"
+          list="$list $tool"
+       else
+	  echo "$tool oK!"
+       fi
+    done
+    if [ "$list" != "" ]; then
+       apt update -y && apt install $list -y
+    fi
 }
 
-echo "bootstrap... "
-bootstrap
-echo "ok!"
+function get_script()
+{
+   rm -rf $SCRIPT && git clone $GIT_URL $SCRIPT -j 4
+}
+
+function link_script()
+{
+   for s in $SCRIPT/*.sh; do
+      local t=$(echo $(basename "$s")|awk -F "." '{print $1}')
+      eval "echo \"${UN_LINK_LIST[*]}\"|grep -qvE \"$t\""
+      [ $? -eq 0 ] && echo -e "\033[5;32mlink -> $t\033[0m" && ln -sf "$s" "$BIN/$t" && continue
+      echo -e "\033[5;33mignore -> $t\033[0m"
+      rm -rf "$BIN/$t"
+   done
+}
+
+function add_config()
+{
+  for n in "$@"; do
+     local c="$SCRIPT/config.sh"
+     local f="$HOME/$n"
+     if [ -e "$f" ]; then
+	local r=$(sed -n "/O_o/p" "$f")
+	#echo $r
+	if [ "$r" != "" ]; then
+           echo -e "\033[5;34mskip -> $n\033[0m" && continue
+        fi
+     fi
+     echo "#[O_o]" >> "$f"
+     echo ". $c" >> "$f"
+     echo -e "\033[5;36mconfig -> $c $2\033[0m"
+  done
+}
+
+app $*
