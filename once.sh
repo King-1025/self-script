cd5c4bdbd919f915cc322b3e7884bd26139d8bf9
King-1025 <1543641386@qq.com>
2020-05-20 12:41:28 +0800

    修改auto_rsync、config、once和trt

--- once.sh
+++ once.sh
@@ -4,11 +4,13 @@
 
 sysdir=$KALI_HOME
 basedir=/root/ME
-java_home=/opt/jdk1.8.0_181 
+java_home=/usr/lib/jvm/java-1.8.0-openjdk-arm64
 classpath=.:$java_home/lib:$java_hone/jre/lib
 gradle_home=/opt/gradle-5.0-rc-4
-android_home=/opt/android-sdk-linux
-path=/bin:/usr/bin:/sbin:/usr/sbin:$java_home/bin:$java_home/jre/bin:$gradle_home/bin
+android_home=/opt/android-sdk
+qemu_home=/opt/qemu
+self=/root/self
+path=/bin:/usr/bin:/sbin:/usr/sbin:$java_home/bin:$java_home/jre/bin:$gradle_home/bin:$qemu_home/bin:/usr/local/bin:$self/bin:/root/.cargo/bin
 extra="-b $SD_0/AppProjects:/root/AIDE"
 
 ld_preload=$LD_PRELOAD
@@ -24,6 +26,6 @@ if [ "$1" = "-p" ]; then
 fi
 #set +x
 
-proot --link2symlink -0 -r $sysdir -b /dev -b /proc -b /sys -b $xp:$basedir $extra -w $basedir /usr/bin/env -i HOME=$basedir USER=root TERM="xterm-256color" LANG=en_US.UTF-8 JAVA_HOME=$java_home CLASSPATH=$classpath GRADLE_HOME=$gradle_home ANDROID_HOME=$android_home PATH=$path $@
+proot --link2symlink -0 -r $sysdir $(sed -n "/^-b/p" $HOME/.kali_dev) -b /proc -b /sys -b $HOME:/termux -b $xp:$basedir $extra -w $basedir /usr/bin/env -i HOME=/root USER=root TERM="xterm-256color" LANG=en_US.UTF-8 JAVA_HOME=$java_home CLASSPATH=$classpath GRADLE_HOME=$gradle_home ANDROID_SDK_ROOT=$android_home SELF=$self GLIVE_EXEC="./script/gff json" PATH=$path $@
 
 export LD_PRELOAD=$ld_preload
